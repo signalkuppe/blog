292b26ef82c59fbe0ba9ca279a86722a30ab4bb0
Matteo Leoni <leoni.matteo@gmail.com>
2013-12-06 15:06:35 +0100

    first commit

---
+++ _posts/2013-11-30-resegone-canalone-comera.markdown
@@ -0,0 +1,9 @@
+---
+layout: post
+title:  "Resegone, Canalone Comera"
+date:   2013-11-30
+categories: alpinismo
+tags: resegone canalone comera valsassina lecco
+---
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, facere deleniti incidunt amet accusantium qui quaerat molestias iste architecto libero. Veritatis, quam, consequatur in quasi aut inventore rerum rem quod.
---
+++ js/cover.js
@@ -0,0 +1,32 @@
+$(document).ready(function()
+{
+
+	function handleResize()
+	{
+	var h = $(window).height();
+	        $('.cover').css({'height':h+'px'});
+	        $('.cover').addClass('is-rendered');
+	}
+
+	function checkGuide ()
+	{
+		$menucontainer.hasClass('is-open') ? $('.cover-guide').hide() : $('.cover-guide').show();
+	}
+
+	handleResize();
+	
+	$(window).resize(function(){
+	    handleResize();
+	});
+
+
+	var $menubutton = $("#menu-link");
+	var $menucontainer = $("#menu-container");
+
+	$menubutton.click(function()
+	{
+		$menucontainer.toggleClass("is-open");
+		checkGuide();
+	})
+
+})
