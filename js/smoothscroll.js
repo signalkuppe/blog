c1951d4fcb4493520cd18e7bb6b4c6c95f8458cd
Matteo Leoni <leoni.matteo@gmail.com>
2013-12-19 17:40:57 +0100

    minor fixes

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,6 +1,23 @@
 $(document).ready(function()
 {
 	$('a').smoothScroll();
+	// scroll tha page after a small delay when arriving from the front page
+	if(window.location.hash=="#start")
+	{
+		window.scrollTo(0, 0);
+		setTimeout(function() {
+ 			$.smoothScroll(
+			{
+	      		scrollElement: null,
+	      		scrollTarget: '#post',
+	      		speed: 700
+	    	});     
+		}, 500);
+
+
+	}
 })
+
+
 
 
