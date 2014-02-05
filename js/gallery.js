89a2d0042bbee2bb1fbc6e4700f1edf0e41ff9cd
Matteo Leoni <leoni.matteo@gmail.com>
2014-02-05 08:42:54 +0100

    new posts

---
+++ js/gallery.js
@@ -0,0 +1,73 @@
+$(document).ready(function()
+{
+
+	var $links = $('.post-gallery-list');
+	var $activate = $('.post-gallery-mobile-link');
+	// non caricare le immagini su device piccole
+	$links.hide();
+
+	var _showImages = function ()
+	{
+		$links.show();
+		$('[data-src]').each(function()
+		{
+			$(this).attr(
+			{
+				'src': $(this).data('src')
+			})
+		})		
+	}
+
+	var _mostraGallery = function (button)
+	{
+		_showImages();
+		button.find('span').text('Nascondi le foto');
+		button.find('i').attr({'class':'icon-eye-blocked'})
+
+	}
+	var _nascondiGallery = function (button)
+	{
+		$links.hide();
+		button.find('i').attr({'class':'icon-camera'})
+		button.find('span').text('Guarda le foto');
+
+	}
+
+	$activate.click(function(e)
+	{
+		e.preventDefault();
+ 		if($(this).hasClass('open'))
+ 		{
+ 			_nascondiGallery($(this));
+
+ 		}
+ 		else
+ 		{
+ 			_mostraGallery($(this));
+ 			$links.addClass('post-gallery-list--mobile');
+ 		}
+ 		$(this).toggleClass("open");
+	})
+
+
+	enquire.register("screen and (min-width:1024px)", {
+
+		// mostra le immagini 
+	    match : function() 
+	    {
+	    	$activate.hide();
+			_mostraGallery($activate);
+			$links.removeClass('post-gallery-list--mobile');
+	    },      
+	     
+	    // nascondi sul resize on mobile                          
+	    unmatch : function()
+	    {
+	    	$activate.show();
+			_nascondiGallery($activate);
+			$links.addClass('post-gallery-list--mobile');
+	    }  
+
+	      
+	});	
+})
---
+++ js/paceconfig.js
@@ -0,0 +1,22 @@
+paceOptions = {
+  elements: {
+    selectors: ['.cover']
+  }
+}
+
+
+function finish ()
+{
+	$(document).ready(function()
+	{
+		
+		setTimeout(function() {
+			  $('.cover-guide').show();
+		      console.log("pagina caricata")
+		}, 500);
+		
+	})
+	
+}
+
+Pace.on('done', finish);
