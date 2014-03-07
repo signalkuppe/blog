0c6444380cf694774fb99f2e5c16b644ce1e80f3
Matteo Leoni <leoni.matteo@gmail.com>
2014-03-07 17:57:26 +0100

    added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    uglify: {
+      my_target: {
+        files: {
+          'js/app.js':
+          [
+            'bower_components/jquery/jquery.min.js',
+            'bower_components/jquery.smooth-scroll/jquery.smooth-scroll.min.js',
+            'bower_components/jQuery-Unorphanize/unorphanize.jquery.min.js',
+            'bower_components/scrollup/js/jquery.scrollUp.min.js',
+            'bower_components/fastclick/lib/fastclick.js',
+            'bower_components/enquire/dist/enquire.min.js',
+            'js/cover.js',
+            'js/orphans.js',
+            'js/image-lightbox.js',
+            'js/gallery.js',
+            'js/smoothscroll.js',
+            'js/print.js',
+            'js/scrollup.js',
+            'js/fastclick.js',
+            'js/web-app-links-safari.js'
+          ]
+        }
+      }
+    }, 
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
