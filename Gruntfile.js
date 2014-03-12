e5d0cfa5372f1af6c41e1ebfaaa46cdeeeeb0360
Matteo Leoni <leoni.matteo@gmail.com>
2014-03-13 00:08:22 +0100

    compression

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
         },
         files:
         {
-          'js/app.js':
+          'js/app.min.js':
           [
             'bower_components/jquery/jquery.min.js',
             'bower_components/jquery.smooth-scroll/jquery.smooth-scroll.min.js',
@@ -39,15 +39,26 @@ module.exports = function(grunt) {
         },
         command: 's3_website push'
       }
-    } 
+    },
+    cssmin: {
+      combine: {
+      options: {
+        banner: '/* Ho compresso il css per salvare circa 25kb :-) */'
+      },
+        files: {
+          'css/app.min.css': ['css/app.css']
+        }
+      }
+    }
   });
 
  
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify','shell']);
-  grunt.registerTask('compress', ['uglify']);
+  grunt.registerTask('compress', ['uglify','cssmin']);
 
 };
