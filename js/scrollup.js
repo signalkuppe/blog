5f1418bde41c6b5a503d529c494bf92644634125
Matteo Leoni <leoni.matteo@gmail.com>
2014-05-28 00:05:42 +0200

    svg icons, new post

--- js/scrollup.js
+++ js/scrollup.js
@@ -2,4 +2,5 @@ $(function () {
     $.scrollUp({
     	scrollText: ''
     });
+    $('#scrollUp').append('<svg class="icon icon-arrow-up" viewBox="0 0 32 32"><use xlink:href="#icon-arrow-up"></use></svg>')
 });
