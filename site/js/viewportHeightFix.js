c78d7c30639412f11df227597a2c7069477bcb8d
Matteo Leoni <leoni.matteo@gmail.com>
2019-05-03 16:51:40 +0200

    test

--- site/js/viewportHeightFix.js
+++ site/js/viewportHeightFix.js
@@ -4,6 +4,6 @@ document.documentElement.style.setProperty('--vh', vh + 'px');
 
 window.addEventListener('resize', () => {
   // We execute the same script as before
-  var vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  // var vh = window.innerHeight * 0.01;
+  // document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
