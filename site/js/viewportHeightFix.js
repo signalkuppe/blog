88a1878d28b185e2052ddc646a0a3d62b98f750c
Matteo Leoni <leoni.matteo@gmail.com>
2019-05-03 17:01:46 +0200

    test

--- site/js/viewportHeightFix.js
+++ site/js/viewportHeightFix.js
@@ -3,7 +3,10 @@ var vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', vh + 'px');
 
 window.addEventListener('resize', () => {
-  // We execute the same script as before
-  // var vh = window.innerHeight * 0.01;
-  // document.documentElement.style.setProperty('--vh', `${vh}px`);
+  var mediaQuery = window.matchMedia("(orientation: portrait)");
+  var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+  if (!iOS || iOS && !mediaQuery.addEventListener.matches) {
+    var vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', vh + 'px');
+  }
 });
