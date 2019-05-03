30af590ddc160ef6f4e4309f250b9299571da74c
Matteo Leoni <leoni.matteo@gmail.com>
2019-05-03 16:31:18 +0200

    fix viewport resize

--- site/js/viewportHeightFix.js
+++ site/js/viewportHeightFix.js
@@ -1,3 +1,9 @@
 // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
 var vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', vh + 'px');
+
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  var vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
