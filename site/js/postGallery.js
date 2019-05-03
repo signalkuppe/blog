64b4a9233001a22c43a8e630003d3bd56acdbfac
Matteo Leoni <leoni.matteo@gmail.com>
2019-05-03 17:12:02 +0200

    test

--- site/js/postGallery.js
+++ site/js/postGallery.js
@@ -4,7 +4,7 @@
       filter: /.*[cloudinary].*/i,
       overlayBackgroundColor: 'rgba(0,0,0,1)',
       noScrollbars: true,
-      bodyClass: 'is-galleryOpen'
+      bodyClass: 'js-is-galleryOpen'
     })
   })
 })()
