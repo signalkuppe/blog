47e476467b84b03c9efb08f0661e4e0bcfb61bc1
Matteo Leoni <leoni.matteo@gmail.com>
2019-05-03 12:43:42 +0200

    layout fixes

--- site/js/postGallery.js
+++ site/js/postGallery.js
@@ -3,12 +3,8 @@
     baguetteBox.run('.c-imageGallery', {
       filter: /.*[cloudinary].*/i,
       overlayBackgroundColor: 'rgba(0,0,0,1)',
-      afterShow: function () {
-        document.body.style.overflowX = 'hidden'
-      },
-      afterHide: function () {
-        document.body.style.overflowX = 'auto'
-      }
+      noScrollbars: true,
+      bodyClass: 'is-galleryOpen'
     })
   })
 })()
