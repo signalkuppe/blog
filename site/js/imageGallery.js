cff545af657d069dabb1d3b8adbbc4429eae1edc
Matteo Leoni <leoni.matteo@gmail.com>
2019-08-06 17:39:13 +0200

    wip

--- site/js/imageGallery.js
+++ site/js/imageGallery.js
@@ -5,10 +5,14 @@
       items: '.c-imageGallery-item'
     })
     baguetteBox.run('.c-imageGallery', {
-      filter: /.*[cloudinary].*/i,
+      filter: /.*[contentful].*/i,
       overlayBackgroundColor: 'rgba(0,0,0,1)',
-      noScrollbars: true,
-      bodyClass: 'js-is-galleryOpen'
+      afterShow: function () {
+        document.body.style.overflow = 'hidden'
+      },
+      afterHide: function () {
+        document.body.style.overflow = 'visible'
+      }
     })
   })
 })()
