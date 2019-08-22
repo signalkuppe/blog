d854e8d564dcc61faf2d0a95d337085cba00efc0
Matteo Leoni <leoni.matteo@gmail.com>
2019-08-22 12:20:00 +0200

    wip

--- site/_data/posts.js
+++ site/_data/posts.js
@@ -47,6 +47,7 @@ const transformPosts = (posts) => { // ad some custom prop
             <figure class="${i % 2 ? 'post-image-odd' : 'post-img-even'}">
               <img 
                   data-src="${node.data.target.fields.file.url}?fit=thumb&w=1440&fm=jpg&fl=progressive&q=70"
+                  src="${node.data.target.fields.file.url}?fit=thumb&w=800&fm=jpg&fl=progressive&q=10"
                   alt="${node.data.target.fields.description}" 
                   class="lazyImg" /> 
                 <figcaption>${node.data.target.fields.title}</figcaption>
