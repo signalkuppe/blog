031172c13399dd32bbea749444e6c67ab94e5ea4
Matteo Leoni <leoni.matteo@gmail.com>
2019-08-27 11:43:28 +0200

    test service worker

--- site/service-worker.js
+++ site/service-worker.js
@@ -12,28 +12,12 @@ if (workbox) {
     })
   )
 
-  // Cache the underlying font files with a cache-first strategy for 1 year.
   workbox.routing.registerRoute(
-    /^https:\/\/fonts\.gstatic\.com/,
-    new workbox.strategies.CacheFirst({
-      cacheName: `${cachePrefix}-google-fonts-webfonts`,
-      plugins: [
-        new workbox.cacheableResponse.Plugin({
-          statuses: [0, 200],
-        }),
-        new workbox.expiration.Plugin({
-          maxAgeSeconds: 60 * 60 * 24 * 365,
-          maxEntries: 30,
-        })
-      ]
-    })
-  )
-  workbox.routing.registerRoute(
-    /\.(?:js|css)$/,
+    /.*(?:cloudfront)\.net/,
     new workbox.strategies.StaleWhileRevalidate({
-      cacheName: `${cachePrefix}static-resources`,
-    })
-  )
+      cacheName: `${cachePrefix}css-js`
+    }),
+  );
 } else {
   console.log(`Service worker non caricato 😬`)
 }
