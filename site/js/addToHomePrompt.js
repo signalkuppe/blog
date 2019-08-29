7ec667c1feb7f3abef4e9d379551acaa4cbf1c5d
Matteo Leoni <leoni.matteo@gmail.com>
2019-08-29 17:26:58 +0200

    prompt

---
+++ site/js/addToHomePrompt.js
@@ -0,0 +1,21 @@
+(function () {
+  document.addEventListener('DOMContentLoaded', (event) => {
+    var button = document.getElementById('js-promptButton')
+    button.style.display = 'none'
+    if (deferredPrompt) { // prompt has been requested (registerServiceWorker.js)
+      button.style.display = 'block'
+      button.addEventListener('click', function(e) {
+        e.preventDefault()
+        deferredPrompt.prompt()
+        deferredPrompt.userChoice.then(function(choiceResult) {
+          if (choiceResult.outcome === 'dismissed') {
+            alert('buuu!!!')
+          } else {
+            alert('yeah!!!')
+          }
+        })
+        deferredPrompt = null
+      })
+    }
+  })
+})()
--- site/js/registerServiceWorker.js
+++ site/js/registerServiceWorker.js
@@ -1,7 +1,17 @@
+
+
 // Check that service workers are supported
 if ('serviceWorker' in navigator) {
+  var deferredPrompt
   // Use the window load event to keep the page load performant
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js');
-  });
+    navigator.serviceWorker.register('/service-worker.js')
+  })
+  // prevent default install prompt
+  window.addEventListener('beforinstallprompt', function (event) {
+    event.preventDefault()
+    deferredPrompt = event
+    return false
+  })
+  
 }
