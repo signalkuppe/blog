e13ef82797c5a23fa9c7e5a91d342e6bea4e9d50
Matteo Leoni <leoni.matteo@gmail.com>
2019-08-30 11:37:53 +0200

    wip

--- site/js/registerServiceWorker.js
+++ site/js/registerServiceWorker.js
@@ -15,3 +15,26 @@ window.addEventListener('beforeinstallprompt', function (event) {
   deferredPrompt = event
   console.log('prompt event prevent', deferredPrompt)
 })
+
+var promptButton = document.getElementById('js-promptButton')
+if (promptButton) {
+  promptButton.style.display = 'none'
+  promptButton.parentElement.style.display = 'none'
+  console.log('addtoHome', deferredPrompt)
+  if (deferredPrompt) { // prompt has been requested (registerServiceWorker.js)
+    promptButton.style.display = 'block'
+    promptButton.parentElement.style.display = 'block'
+    promptButton.addEventListener('click', function(e) {
+      e.preventDefault()
+      deferredPrompt.prompt()
+      deferredPrompt.userChoice.then(function(choiceResult) {
+        if (choiceResult.outcome === 'dismissed') {
+          alert('buuu!!!')
+        } else {
+          alert('yeah!!!')
+        }
+      })
+      deferredPrompt = null
+    })
+  }
+}
