3f4f62f25a646d4b8b7d05cd6e38369f4d90341b
Matteo Leoni <leoni.matteo@gmail.com>
2020-01-16 08:50:50 +0100

    dep bump, fix gallery

--- site/js/imageGallery.js
+++ site/js/imageGallery.js
@@ -1,19 +1,11 @@
-(function () {
-  new Colcade( '.c-imageGallery', {
-    columns: '.c-imageGallery-col',
-    items: '.c-imageGallery-item'
-  })
-  baguetteBox.run('.js-gallery', {
+(function() {
+  new Colcade(".c-imageGallery", {
+    columns: ".c-imageGallery-col",
+    items: ".c-imageGallery-item"
+  });
+  baguetteBox.run(".js-gallery", {
     filter: /.*[contentful].*/i,
-    overlayBackgroundColor: 'rgba(0,0,0,1)',
-    noScrollbars: true,
-    afterShow: function () {
-      document.body.style.height = '100vh'
-      document.body.style.overflow = 'hidden'
-    },
-    afterHide: function () {
-      document.body.style.height = 'auto'
-      document.body.style.overflow = 'visible'
-    }
-  })
-})()
+    overlayBackgroundColor: "rgba(0,0,0,1)",
+    noScrollbars: true
+  });
+})();
