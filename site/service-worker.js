16c9c491d905f243c2a8d99701c268565f0943e3
Matteo Leoni <leoni.matteo@gmail.com>
2020-04-05 17:46:17 +0200

    inline css

--- site/service-worker.js
+++ site/service-worker.js
@@ -1,5 +1,5 @@
 importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
+  "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
 );
 
 if (workbox) {
