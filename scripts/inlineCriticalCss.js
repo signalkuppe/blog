a1b0bc26055b627bf9901b53c140d995360034f2
Matteo Leoni <leoni.matteo@gmail.com>
2020-04-05 11:51:03 +0200

    test critical

--- scripts/inlineCriticalCss.js
+++ scripts/inlineCriticalCss.js
@@ -1,7 +1,9 @@
 const path = require("path");
 const fs = require("fs");
 const critical = require("critical");
+const cheerio = require("cheerio");
 const glob = require("glob");
+const _ = require("lodash");
 const log = require(path.join(process.cwd(), "lib/log"));
 let i = 0;
 // options is optional
@@ -17,14 +19,31 @@ glob(
       files.forEach(async (file) => {
         try {
           const { css, html, uncritical } = await critical.generate({
-            inline: true,
+            inline: false,
             base: "dist",
             minify: true,
             src: file,
             width: 1300,
             height: 900,
           });
-          fs.writeFileSync(path.join(process.cwd(), "dist", file), html);
+          console.log(file);
+          const oldFile = fs.readFileSync(path.join("dist", file)).toString();
+          const $ = cheerio.load(oldFile);
+          $("link[rel='stylesheet']").map((i, link) => {
+            const stylesheet = "this.rel='stylesheet'";
+            $(link).attr("rel", "preload");
+            $(link).attr("as", "style");
+            $(link).attr("onload", stylesheet);
+          });
+          $("head").append(`<style media="screen">${css}</style>`);
+          fs.writeFileSync(
+            path.join("dist", file),
+            _.replace(
+              $.html(),
+              new RegExp("this.rel=&apos;stylesheet&apos;", "g"),
+              "this.rel='stylesheet'"
+            )
+          );
           log.info(`inlined css for ${file}`);
           i++;
           if (i === files.length) {
