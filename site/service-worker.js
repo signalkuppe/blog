5781aecf711752c16716fd848fc4032bade8a72f
Matteo Leoni <leoni.matteo@gmail.com>
2020-04-05 17:50:15 +0200

    inline css

--- site/service-worker.js
+++ site/service-worker.js
@@ -1,32 +1,36 @@
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
-);
-
-if (workbox) {
-  console.log(`Service worker loaded 🎉`);
-
-  workbox.core.skipWaiting();
-  workbox.core.clientsClaim();
-  workbox.precaching.precacheAndRoute([]); // filled by service-worker-build.js
-
-  // Use a StaleWhileRevalidate strategy as default
-  workbox.routing.setDefaultHandler(
-    new workbox.strategies.StaleWhileRevalidate({
-      cacheName: "signalkuppe",
-    })
+try {
+  importScripts(
+    "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
   );
 
-  // This "catch" handler is triggered when any of the other routes fail to generate a response
-  workbox.routing.setCatchHandler(({ event }) => {
-    switch (event.request.destination) {
-      case "document":
-        return caches.match(
-          workbox.precaching.getCacheKeyForURL("offline.html")
-        ); // serve the precached offline page
-      default:
-        return Response.error();
-    }
-  });
-} else {
-  console.log(`Service worker can’t be loaded 😬`);
+  if (workbox) {
+    console.log(`Service worker loaded 🎉`);
+
+    workbox.core.skipWaiting();
+    workbox.core.clientsClaim();
+    workbox.precaching.precacheAndRoute([]); // filled by service-worker-build.js
+
+    // Use a StaleWhileRevalidate strategy as default
+    workbox.routing.setDefaultHandler(
+      new workbox.strategies.StaleWhileRevalidate({
+        cacheName: "signalkuppe",
+      })
+    );
+
+    // This "catch" handler is triggered when any of the other routes fail to generate a response
+    workbox.routing.setCatchHandler(({ event }) => {
+      switch (event.request.destination) {
+        case "document":
+          return caches.match(
+            workbox.precaching.getCacheKeyForURL("offline.html")
+          ); // serve the precached offline page
+        default:
+          return Response.error();
+      }
+    });
+  } else {
+    console.log(`Service worker can’t be loaded 😬`);
+  }
+} catch (err) {
+  console.log("sw", err);
 }
