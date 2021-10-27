74c603f118937aa13c49528142c9bc12bc605b8f
Matteo Leoni <leoni.matteo@gmail.com>
2021-10-27 17:39:22 +0200

    wip

---
+++ src/pages/portfolio.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import vars from '../vars';
+import BaseLayout from '../components/layout/Base';
+import Head from '../components/common/Head';
+
+export const permalink = '/portfolio.html';
+
+export default function Portfolio({ route }) {
+    return <div>test</div>;
+}
