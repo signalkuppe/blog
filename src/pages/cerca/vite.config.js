f7b918360095d655bc0c16df64dc480f13adc361
signalkuppe <leoni.matteo@gmail.com>
2022-07-28 10:37:42 +0200

    fix

--- src/pages/cerca/vite.config.js
+++ src/pages/cerca/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import htmlPlugin from './vite-plugin-html'; // to modify index.html at build time, copied from here https://github.com/ahwgs/vite-plugin-html-config nad modified
 import vars from '../../vars';
+import { vars as cssVars } from '../../theme/index';
 
 // add meta tag to index.html at build time
 const htmlPluginOpt = {
@@ -14,7 +15,7 @@ const htmlPluginOpt = {
         },
     ],
     title: `Cerca una relazione | ${vars.siteName}`,
-    style: `body { color: var(--color-background); };`,
+    style: `body { background: ${cssVars['--color-background']} };`,
 };
 
 // https://vitejs.dev/config/
