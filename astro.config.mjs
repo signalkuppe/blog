8c8efaa54cec93d31201e61d7d4283ab53fbbaf5
signalkuppe <leoni.matteo@gmail.com>
2023-12-15 17:22:07 +0100

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-	integrations: [react()]
-})
+export default defineConfig({});
