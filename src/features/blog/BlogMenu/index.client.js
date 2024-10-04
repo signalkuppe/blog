e1804f3fef78380ffe8149aa81d4df732b01e0ed
signalkuppe <leoni.matteo@gmail.com>
2024-10-04 11:59:04 +0200

    wip

--- src/features/blog/BlogMenu/index.client.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelectorAll('.js-blogMenu-item');
-Array.from(menu).forEach((item) => (item.style.display = 'none'));
-document.fonts.ready.then(function () {
-    // prevent menu layout shifts
-    Array.from(menu).forEach((item) => item.removeAttribute('style'));
-});
