d5f7d752a932af287a445787b02807dd6c24ba97
signalkuppe <leoni.matteo@gmail.com>
2024-10-23 17:41:28 +0200

    wip

--- src/lib/contentful.js
+++ src/lib/contentful.js
@@ -1,10 +1,43 @@
-import * as contentful from 'contentful'
-
+import * as contentful from "contentful";
+import { union, groupBy } from "underscore";
 
 export const contentfulClient = contentful.createClient({
   space: import.meta.env.SIGNALKUPPE_WEBSITE_CONTENTFUL_SPACE,
-  accessToken: import.meta.env.NODE_ENV === 'development'
-    ? import.meta.env.SIGNALKUPPE_WEBSITE_CONTENTFUL_PREVIEWTOKEN
-    : import.meta.env.SIGNALKUPPE_WEBSITE_CONTENTFUL_ACCESSTOKEN,
-  host: import.meta.env.NODE_ENV === 'development' ? "preview.contentful.com" : "cdn.contentful.com",
-})
+  accessToken:
+    import.meta.env.NODE_ENV === "development"
+      ? import.meta.env.SIGNALKUPPE_WEBSITE_CONTENTFUL_PREVIEWTOKEN
+      : import.meta.env.SIGNALKUPPE_WEBSITE_CONTENTFUL_ACCESSTOKEN,
+  host:
+    import.meta.env.NODE_ENV === "development"
+      ? "preview.contentful.com"
+      : "cdn.contentful.com",
+});
+
+export async function getPosts() {
+  const query = {
+    content_type: "post",
+    include: 1,
+    order: "-fields.date",
+    limit: 1,
+    skip: 0,
+  };
+  const perIteration = 20;
+  let iteration = 1;
+  let posts;
+  query.limit = perIteration;
+  query.skip = 0;
+  let chunk = await contentfulClient.getEntries(query);
+  posts = chunk.items;
+  while (chunk.total > query.limit * iteration) {
+    query.skip = query.limit * iteration;
+    chunk = await contentfulClient.getEntries(query);
+    posts = union(posts, chunk.items);
+    iteration++;
+  }
+
+  const categories = [
+    ...new Set(posts.flatMap((item) => item.fields.category)),
+  ];
+
+  return { posts: posts.map((p) => ({ ...p, allCategories: categories })) };
+}
