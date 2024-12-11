cf898f59f88d27ae7da139d1829bf17fcaf7fb70
signalkuppe <leoni.matteo@gmail.com>
2024-12-11 09:28:42 +0100

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.signalkuppe.com",
   outDir: ".site",
   devToolbar: { enabled: false },
   prefetch: true,
-  redirects: {
-    "/portfolio": "/portfolio/1",
-  },
+  adapter: netlify(),
 });
