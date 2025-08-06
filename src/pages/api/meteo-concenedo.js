6ae9fbdcbdddc08651b70ec929aac88f6ff76408
signalkuppe <leoni.matteo@gmail.com>
2025-08-06 10:23:36 +0200

    better api

---
+++ src/pages/api/meteo-concenedo.js
@@ -0,0 +1,46 @@
+// api per meteo-valsassina
+
+import weatherlinkData from "../../lib/weatherlink.js";
+export async function GET() {
+  let data;
+  try {
+    data = await weatherlinkData();
+  } catch (error) {
+    console.error("Weatherlink error:", error); // Log the error for debugging
+    return new Response(JSON.stringify(error), {
+      status: 400,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+  }
+
+  return new Response(
+    JSON.stringify({
+      name: "Concenedo",
+      lastUpdate: `${data.current.last_data_day} ${data.current.last_data_hour}`,
+      temperature: parseFloat(data.current.temperature),
+      temperatureMin: parseFloat(data.day.temperature_min),
+      temperatureMinTime: data.day.temperature_min_at,
+      temperatureMax: parseFloat(data.day.temperature_max),
+      temperatureMaxTime: data.day.temperature_max_at,
+      humidity: parseFloat(data.current.humidity),
+      humidityMax: parseFloat(data.day.humidity_max),
+      humidityMaxTime: data.day.humidity_max_at,
+      humidityMin: parseFloat(data.day.humidity_min),
+      humidityMinTime: data.day.humidity_min_at,
+      windSpeed: parseFloat(data.current.wind),
+      windGust: parseFloat(data.day.wind_max),
+      windGustTime: data.day.wind_max_at,
+      windGustDirection: data.day.wind_max_dir,
+      windGustDirectionTime: data.day.wind_max_at,
+      coords: [45.5631, 9.2842],
+    }),
+    {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    }
+  );
+}
