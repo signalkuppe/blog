25cb14ef88e1e97bb5e000b747fb73c29d9a3fc1
signalkuppe <leoni.matteo@gmail.com>
2026-05-05 14:37:33 +0200

    sentry + fix

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -2,7 +2,6 @@ import * as Sentry from "@sentry/astro";
 
 Sentry.init({
   dsn: "https://e600a6f00828820db424b01363ba28f3@o4508455479672832.ingest.de.sentry.io/4511337208152144",
-  // Adds request headers and IP for users, for more info visit:
-  // https://docs.sentry.io/platforms/javascript/guides/astro/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
+  sendDefaultPii: false,
+  autoSessionTracking: false,
 });
